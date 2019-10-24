e19730ae27790a8e501b5b62ee9edddb9b0490d7
Matilda <matildaytc@gmail.com>
2019-10-24 09:56:41 +0100

    Changed the database content and made few modifications for the tables and api

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,22 +1,38 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS deposit;
 CREATE TABLE IF NOT EXISTS user (
   firstName VARCHAR(255) NOT NULL,
   lastName VARCHAR(255),
   birthday DATE,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(60) NOT NULL,
+  balance VARCHAR(255) NOT NULL,
+  picture VARCHAR(255),
   UNIQUE(email)
 );
-CREATE TABLE IF NOT EXISTS product (
+CREATE TABLE IF NOT EXISTS deposit (
+  depositId Integer primary key autoincrement,
   ownerId VARCHAR(255) NOT NULL,
+  productId INT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS product (
+  id integer primary key autoincrement,
   title VARCHAR(255) NOT NULL,
-  amount INT NOT NULL,
-  price INT NOT NULL,
-  open BOOLEAN NOT NULL DEFAULT 0
+  startingPrice INT NOT NULL,
+  rate FLOAT NOT NULL,
+  variance FLOAT NOT NULL
 );
 INSERT INTO
-  product (ownerId, title, amount, price, open)
+  deposit (ownerId, productId)
 VALUES
-  ("test@gmail.com", "Pikachu", 10, 100, 1),
-  ("test@gmail.com", "Raichu", 5, 400, 0);
+  ("test@gmail.com", 1),
+  ("test@gmail.com", 2);
+INSERT INTO
+  product (title, startingPrice, rate, variance)
+VALUES
+  ("Pikachu", 100, 1.001, 0.4),
+  ("Mewto", 250, 1.005, 0.2),
+  ("Pidgey", 30, 1.002, 0.9),
+  ("Charizard", 700, 1.008, 0.1),
+  ("Blastoise", 1000, 1.003, 0.5);
